382e602551926d67ee2dc5b523abc220aebda6ca
name <82998776+sener7106@users.noreply.github.com>
2021-09-16 20:24:03 +0900

    12.0 urls and templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import EmptyPage, Paginator
+from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
-# Create your views here.
-# context -> 변수를 보내는 방법 중 하나
 
+class HomeView(ListView):
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+    """Home View Definition"""
 
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
     
+    """  def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        now = timezone.now()
+        context["now"] = now
+        return context """ 
+        # function based view
